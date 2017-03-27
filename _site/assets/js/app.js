24e7e768a1e10eff2354539cae3af076fe6ba80d
Gareth Johns <info@garethjohnsdesign.com>
2017-03-27 16:39:01 +0100

    Initial Commit

--- _site/assets/js/app.js
+++ _site/assets/js/app.js
@@ -3,3 +3,15 @@ $(document).foundation();
 $('body').smoothScroll({
   delegateSelector: '.top-bar a'
 });
+
+
+wow = new WOW(
+  {
+  boxClass:     'wow',      // default
+  animateClass: 'animated', // default
+  offset:       0,          // default
+  mobile:       true,       // default
+  live:         true        // default
+}
+)
+wow.init();
